2266e91543516b13bcd76379fec744cc9d0f2102
Francois Harbec <fharbec@gmail.com>
2017-05-11 16:36:58 -0700

    simplify

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,15 @@
 class Item
- 
-  PREFIX_NAMES = ["Smoke","Weed","Fresh","Dank","Fiery","Burning","The","Chilling","Freezing","The Devil's","Big Smoke's","Heart of the",
-"Blazing","Shadowed","Saltbringer","Tranquil","Edgy","Heartbreaker","Gaseous","Sturdy","Hugh Mungus","Weightless","Lucky","Burned", "Warped","Swift",
-"Heavy","Boosted","Bloodsucker's","Greased","Glowing 1000 Degree","Rapid","Solid","Filthy",
-"Damaged","Dented","Nefarious","Arcadian","Baleful","Adamant","Lame","Big","Spasmodic","Liquid",
-"Sam West's","Steaming","Fuming","Smoking","Polished","Bent","Painful","Chippped","Cracked",
-"Legendary","Heroic","Dangerous","Boring","Zealous","Redolent","Execrable","Insolent","Tenacious","Turgid","Sagacious","Spritely","Boorish",
-"Robust","Well-worn","Spicy","Angry","Joyous","Humming","Crystallized","Dapper","Direful","Erratic","Verdant","Tawdry","Placid","Macabre",
-"Searing","Cold","Singing","Stalwart","Dastardly","Dusty","Sparking","Charged","Thunderbolt","Icy","Frosty","Chilly","Acidic","Poisonous","Venomous",
-"Toxic","Radiant","Solar","Lunar","Deadly","Haunting","Spooky","Rusted","Depressed","Alluring","Valorous","Strong","Hardy","Luminous","Furious",
-"Unfortunate","Disgusting","Quentin's","Vine","Grass","Waxed","Powerful","Nimble","Lustrous","Sparkling"]
-   
-   
-    
+  attr_reader :name, :weapon
+
+  PREFIX_NAMES = [
+    {name: "Smoke", colour: "#ffee00"},
+    {name: "Weed", colour: "#00ff00"},
+    {name: "Fresh", colour: "#0000ff"},
+    {name: "Dank", colour: "purple"},
+    {name: "Foo", colour: nil }
+  ]
+
+
   WEAPON_NAMES = ["Massive","Mate","Blade","Sword","Axe","Crossbow","Bow",
 "Broadsword","Greatbow","Hand Cannon","Destructor","Bludgeoner","Flail","Heavy Flail","Morning Star","Greatsword","Bastard Sword",
 "Scimitar","Katana","Bokken","Tachi","OOOHHHHH","Songblade","Kunai",
@@ -29,13 +26,23 @@ class Item
 "Shrapnel Cannon","Spice Bomb Launcher","Blitz Gun","Chopper","Bladed Chakram","Stake launcher","Boomerang","Lunarang",
 "Heavy Chakram","Railgun","Beamblade","Damb","Trench Shovel","Javelin"]
 
-  def random_prefix
-    @prefix = PREFIX_NAMES.sample #Selects a single name from the PREFIX_NAMES array
-  end
-  
-  def random_weapon
-    @weapon = WEAPON_NAMES.sample #Selects a single name from the WEAPON_NAMES array
+
+  def initialize
+    @prefix = PREFIX_NAMES.sample
+    @colour = @prefix[:colour]
+    @name = @prefix[:name]
+    @weapon = WEAPON_NAMES.sample
   end
 
+  def description
+    "#{name} #{weapon}"
+  end
 
+  def colour
+    if @colour.nil?
+      "#ffffff"
+    else
+      @colour
+    end
+  end
 end
